97cb265af891a35bc8ec3fc2ac56e399425355e9
rizchi17 <rizchi17ub@gmail.com>
2025-09-23 14:35:47 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/home_widget_sandbox/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.home_widget_sandbox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ios/HomeWidget/HomeWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  HomeWidgetBundle.swift
+//  HomeWidget
+//
+//  Created by 稲山千穂 on 2025/09/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HomeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        HomeWidget()
+    }
+}
